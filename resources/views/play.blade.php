847f6b4f2c8d2b3664706b21ce99531d8599756a
Kamal Kamalaldin <ask@kamalaldin.com>
2015-11-29 22:03:34 -0500

    Just One more fix! Fixed Redirect in introduction, I think/hope..server testing in progress.

--- resources/views/play.blade.php
+++ resources/views/play.blade.php
@@ -1,6 +1,6 @@
 @extends('response')
 @section('response-body')
-    <Gather timeout="5" finishOnKey="*" action="../result" method="POST">
+    <Gather timeout="5" finishOnKey="*" action="/buzzphone/voice/result" method="POST">
         <Say voice="woman" language="en">{!! $say_text !!} </Say>
     </Gather>
     <Redirect method="POST">
